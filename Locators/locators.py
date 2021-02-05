0c9ea9f9b67ff8a4b1297bdcac32b36f6097947c
Olu <olujs1999@gmail.com>
2021-02-05 19:22:05 +0000

    updated version of chromedriver, locators for mac OS and added requirements file

--- Locators/locators.py
+++ Locators/locators.py
@@ -7,17 +7,29 @@ class locators:
     searches_button_xpath = "//button[@id='header-desktop-search-button']"
 
     # statsPage
+    # Commented out selectors is for Windows OS
     stockName_text_cssSelector = ".Fz\(18px\)"
-    forwardPE_text_cssSelector = ".Bxz\(bb\):nth-child(4) > .Ta\(c\):nth-child(2)"
-    marketCap_text_cssSelector = ".BdY > .Ta\(c\):nth-child(2)"
-    enterpriseValue_text_cssSelector = ".Bxz\(bb\):nth-child(2) > .Ta\(c\):nth-child(2)"
+    # forwardPE_text_cssSelector = ".Bxz\(bb\):nth-child(4) > .Ta\(c\):nth-child(2)"
+    forwardPE_text_cssSelector = ".fi-row:nth-child(4) > .Fw\(500\)"
+    # marketCap_text_cssSelector = ".M\(0\) .Bxz\(bb\):nth-child(1) > .Ta\(end\)"
+    marketCap_text_cssSelector = ".fi-row:nth-child(1) > .Fw\(500\)"
+    # enterpriseValue_text_cssSelector = ".Bxz\(bb\):nth-child(2) > .Ta\(c\):nth-child(2)"
+    enterpriseValue_text_cssSelector = ".fi-row:nth-child(2) > .Fw\(500\)"
+    # totalCash_text_cssSelector = ".Pos\(r\):nth-child(5) .Bxz\(bb\):nth-child(1) > .Fw\(500\)"
     totalCash_text_cssSelector = ".Pos\(r\):nth-child(5) .Bxz\(bb\):nth-child(1) > .Fw\(500\)"
+    # totalDebt_text_cssSelector = ".Pos\(r\):nth-child(5) .Bxz\(bb\):nth-child(3) > .Fw\(500\)"
     totalDebt_text_cssSelector = ".Pos\(r\):nth-child(5) .Bxz\(bb\):nth-child(3) > .Fw\(500\)"
+    # dividend_text_cssSelector = ".Pstart\(20px\) > .Pos\(r\):nth-child(3) .Bxz\(bb\):nth-child(2) > .Fw\(500\)"
     dividend_text_cssSelector = ".Pstart\(20px\) > .Pos\(r\):nth-child(3) .Bxz\(bb\):nth-child(2) > .Fw\(500\)"
+    # dividend5Year_text_cssSelector = ".Pos\(r\):nth-child(3) > div > div > .W\(100\%\) .Bxz\(bb\):nth-child(5) > .Fw\(500\)"
     dividend5Year_text_cssSelector = ".Pos\(r\):nth-child(3) > div > div > .W\(100\%\) .Bxz\(bb\):nth-child(5) > .Fw\(500\)"
+    # payoutRatio_text_cssSelector = ".Pos\(r\):nth-child(3) > div > div > .W\(100\%\) .Bxz\(bb\):nth-child(6) > .Fw\(500\)"
     payoutRatio_text_cssSelector = ".Pos\(r\):nth-child(3) > div > div > .W\(100\%\) .Bxz\(bb\):nth-child(6) > .Fw\(500\)"
+    # quarterlyRevenueGrowth_text_cssSelector = ".Pos\(r\):nth-child(4) .Bxz\(bb\):nth-child(3) > .Fw\(500\)"
     quarterlyRevenueGrowth_text_cssSelector = ".Pos\(r\):nth-child(4) .Bxz\(bb\):nth-child(3) > .Fw\(500\)"
+    # quarterlyEarningsGrowth_text_cssSelector = ".Pos\(r\):nth-child(4) .Bxz\(bb\):nth-child(8) > .Fw\(500\)"
     quarterlyEarningsGrowth_text_cssSelector = ".Pos\(r\):nth-child(4) .Bxz\(bb\):nth-child(8) > .Fw\(500\)"
+
 
     # stockPage
     stats_link_xpath = "//span[contains(text(),'Statistics')]"
